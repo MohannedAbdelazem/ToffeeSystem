34d8c7b74b0ee102cefa163461982b6dadc44122
Mohanned <mohannedabdelazem222@gmail.com>
2023-05-07 19:25:56 +0300

    updates

---
+++ toffeeSystem/src/Admin.java
@@ -0,0 +1,54 @@
+public class Admin {
+    private int id;
+    private String name;
+    private char gender;
+    private String address;
+    private String Email;
+    Admin(int id, String name, char gender, String address, String email){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.Email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
--- toffeeSystem/src/Customer.java
+++ toffeeSystem/src/Customer.java
@@ -1,6 +1,58 @@
-import java.util.ArrayList;
-import java.sql.*;
-import java.io.*;
 public class Customer extends unregisteredUser {
+    private int CustomerID;
+    private String name;
+    private int age;
+    private char Gender;
+    private String address;
+    private String email;
+    private int loyalityPoints;
+    Customer(int ID, String Name, int Age, char G, String Address, String Email, int loyalityPoints){
+        CustomerID = ID;
+        this.name = Name;
+        this.age = Age;
+        this.Gender = G;
+        this.address = Address;
+        this.email = Email;
+        this.loyalityPoints = loyalityPoints;
+    }
+    int getID(){
+        return this.CustomerID;
+    }
+    void setName(String Name){
+        this.name = Name;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setAge(int Age){
+        this.age = Age;
+    }
+    int getAge(){
+        return this.age;
+    }
+    void SetGender(char G){
+        this.Gender = G;
+    }
+    char getGender(){
+        return this.Gender;
+    }
+    String getAddress(){
+        return this.address;
+    }
+    void updateAddress(String newAddress){
+        this.address = newAddress;
+    }
+    void updateEmail(String Email){
+        this.email = Email;
+    }
+    String getEmail(){
+        return this.email;
+    }
+    void setLoyalityPoints(int LP){
+        this.loyalityPoints = LP;
+    }
+    int getloyalityPoints(){
+        return this.loyalityPoints;
+    }
 
 }
